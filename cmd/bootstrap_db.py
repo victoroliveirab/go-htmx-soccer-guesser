3df395398a05185fb4e7025caf23d31c715a6fbe
Victor Oliveira <eu@victoroliveira.com.br>
2024-05-07 22:12:16 -0300

    chore: updates bootstrap db script to always use current dates

--- cmd/bootstrap_db.py
+++ cmd/bootstrap_db.py
@@ -1,5 +1,13 @@
 import os
 import sqlite3
+from datetime import datetime
+
+
+def offset_days(dt, offset=0):
+    timestamp = dt.timestamp()
+    new_timestamp = timestamp + offset * 3600 * 24
+    return datetime.fromtimestamp(new_timestamp)
+
 
 SCHEMA_FILE = "scripts/create_schema.sql"
 USER_GROUP_FILE = "scripts/create_users_and_groups.sql"
@@ -63,8 +71,90 @@ with open(FIXTURES_FILE, "r") as f:
 
 cursor.executescript(sql_script)
 conn.commit()
-
 print("Created fixtures")
+
+# 6.1 Update fixtures to mix between past, current, and future
+# 9 past (5 two days ago + 4 yesterday)
+# 2 current
+# 9 future (5 tomorrow + 4 two days from today)
+
+now = datetime.now()
+for i in range(20):
+    row_id = i + 1
+    if i < 5:  # two days ago
+        two_days_ago = offset_days(now, -2)
+        two_days_ago = two_days_ago.replace(minute=30, second=0, microsecond=0)
+
+        timestamp_numb = int(two_days_ago.timestamp() * 1000)
+        match_date = "T".join(str(two_days_ago).split(" ")) + "+00:00"
+        cursor.execute(
+            """
+            UPDATE fixtures
+            SET timestamp_numb = ?,
+                match_date = ?
+            WHERE id = ?
+        """,
+            (timestamp_numb, match_date, row_id),
+        )
+    elif i < 9:  # yesterday
+        yesterday = offset_days(now, -1)
+        yesterday = yesterday.replace(minute=0, second=0, microsecond=0)
+
+        timestamp_numb = int(yesterday.timestamp() * 1000)
+        match_date = "T".join(str(yesterday).split(" ")) + "+00:00"
+        cursor.execute(
+            """
+            UPDATE fixtures
+            SET timestamp_numb = ?,
+                match_date = ?
+            WHERE id = ?
+        """,
+            (timestamp_numb, match_date, row_id),
+        )
+    elif i < 11:  # now
+        today = now.replace(minute=0, microsecond=0)
+        timestamp_numb = int(today.timestamp() * 1000)
+        match_date = "T".join(str(today).split(" ")) + "+00:00"
+        cursor.execute(
+            """
+            UPDATE fixtures
+            SET timestamp_numb = ?,
+                match_date = ?
+            WHERE id = ?
+        """,
+            (timestamp_numb, match_date, row_id),
+        )
+    elif i < 16:
+        tomorrow = offset_days(now, 1)
+        tomorrow = tomorrow.replace(minute=30, second=0, microsecond=0)
+        timestamp_numb = int(tomorrow.timestamp() * 1000)
+        match_date = "T".join(str(tomorrow).split(" ")) + "+00:00"
+        cursor.execute(
+            """
+            UPDATE fixtures
+            SET timestamp_numb = ?,
+                match_date = ?
+            WHERE id = ?
+        """,
+            (timestamp_numb, match_date, row_id),
+        )
+    else:
+        two_days_ahead = offset_days(now, 2)
+        two_days_ahead = two_days_ahead.replace(minute=30, second=0, microsecond=0)
+        timestamp_numb = int(two_days_ahead.timestamp() * 1000)
+        match_date = "T".join(str(two_days_ahead).split(" ")) + "+00:00"
+        cursor.execute(
+            """
+            UPDATE fixtures
+            SET timestamp_numb = ?,
+                match_date = ?
+            WHERE id = ?
+        """,
+            (timestamp_numb, match_date, row_id),
+        )
+
+print("Update Fixtures to current date")
+
 
 # 7- Create guesses
 with open(GUESSES_FILE, "r") as f:
