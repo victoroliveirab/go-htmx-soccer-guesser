76c92f385f9c45b06cf2226d25bdd7cd585d1012
Victor Oliveira <eu@victoroliveira.com.br>
2024-05-30 13:13:51 -0300

    chore: adds more time to session cookie for my sanity

--- config/auth.go
+++ config/auth.go
@@ -2,6 +2,6 @@ package config
 
 const (
 	SessionCookieName   = "session_id"
-	SessionCookieMaxAge = 60 * 60 * 24 * 30
+	SessionCookieMaxAge = 200 * 60 * 60 * 24 * 30
 	SessionCookiePath   = "/"
 )
