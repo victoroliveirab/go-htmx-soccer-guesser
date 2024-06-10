5d620e3719083a8b06b5ba0734f2894e9b39da39
Victor Oliveira <eu@victoroliveira.com.br>
2024-06-09 21:54:23 -0300

    feat: adds spinner and disabling to navigation buttons

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,14 @@
-/** @type HTMLDivElement */
-const mobileMenuButton = document.getElementById("mobile-menu-button");
-/** @type HTMLDivElement */
-const mobileMenu = document.getElementById("mobile-menu");
+(function () {
+    /** @type HTMLDivElement */
+    const mobileMenuButton = document.getElementById("mobile-menu-button");
+    /** @type HTMLDivElement */
+    const mobileMenu = document.getElementById("mobile-menu");
 
-mobileMenuButton.addEventListener("click", () => {
-    const svgs = mobileMenuButton.getElementsByTagName("svg");
-    for (const svg of svgs) {
-        svg.classList.toggle("hidden");
-    }
-    mobileMenu.children[0].classList.toggle("hidden");
-});
+    mobileMenuButton.addEventListener("click", () => {
+        const svgs = mobileMenuButton.getElementsByTagName("svg");
+        for (const svg of svgs) {
+            svg.classList.toggle("hidden");
+        }
+        mobileMenu.children[0].classList.toggle("hidden");
+    });
+})();
