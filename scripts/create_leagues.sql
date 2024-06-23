de8605147f255d6c8d79e5df9291ccd93446d43d
Victor Oliveira <eu@victoroliveira.com.br>
2024-06-23 13:30:54 -0300

    chore: adds more cups to the league script

--- scripts/create_leagues.sql
+++ scripts/create_leagues.sql
@@ -2,10 +2,14 @@ INSERT INTO Leagues (api_football_id, name, logo_url, country, country_flag_url,
 (71, 'Série A', 'https://media.api-sports.io/football/leagues/71.png', 'Brazil', 'https://media.api-sports.io/flags/br.svg', 'league'),
 (72, 'Série B', 'https://media.api-sports.io/football/leagues/72.png', 'Brazil', 'https://media.api-sports.io/flags/br.svg', 'league'),
 (73, 'Copa do Brasil', 'https://media.api-sports.io/football/leagues/73.png', 'Brazil', 'https://media.api-sports.io/flags/br.svg', 'cup'),
-(75, 'Série C', 'https://media.api-sports.io/football/leagues/75.png', 'Brazil', 'https://media.api-sports.io/flags/br.svg', 'league');
+(75, 'Série C', 'https://media.api-sports.io/football/leagues/75.png', 'Brazil', 'https://media.api-sports.io/flags/br.svg', 'league'),
+(4, 'Euro Championship', 'https://media.api-sports.io/football/leagues/4.png', 'World', NULL, 'cup'),
+(9, 'Copa América', 'https://media.api-sports.io/football/leagues/9.png', 'World', NULL, 'cup');
 
 INSERT INTO Leagues_Seasons (league_id, season) VALUES
 (1, 2024),
 (2, 2024),
 (3, 2024),
-(4, 2024);
+(4, 2024),
+(5, 2024),
+(6, 2024);
