66912a057310b52ba1426bcfebd86088e66095da
Victor Oliveira <eu@victoroliveira.com.br>
2024-06-22 23:41:09 -0300

    feat: adds extra spacing token to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["components/**/*.html", "views/**/*.html", "static/js/*.js"],
+    theme: {
+        extend: {
+            spacing: {
+                128: "32rem",
+            },
+        },
+    },
 };
