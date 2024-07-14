200c4038704d8f81503e4d7ba923774936fe96ed
Victor Oliveira <eu@victoroliveira.com.br>
2024-07-14 19:02:15 -0300

    chore: fixes js path on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["components/**/*.html", "views/**/*.html", "static/js/*.js"],
+    content: ["components/**/*.html", "views/**/*.html", "static/js/**/*.js"],
     theme: {
         extend: {
             spacing: {
